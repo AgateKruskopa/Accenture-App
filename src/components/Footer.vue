a3b77866c270da8379da1fae4143a5509e516c6f
Agate Kruskopa <agate.kruskopa@gmail.com>
2021-08-22 16:29:56 +0300

    Vue_setup

---
+++ src/components/Footer.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <ul id="footer-container">
+            <li><a href="/" target="_self">Contact Us</a></li>
+            <li></li>
+            <li></li>
+        </ul>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Footer',
+    props: {
+            subscription: String,
+            logo: String
+    },
+}
+</script>
+
+<style>
+#footer-container{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    list-style: none;
+    padding: 1rem;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,101 @@
+//template
+<template>
+    <header>
+        <img id="logo" alt="Vue logo" src="../assets/AA_logo.jpg">
+        <ul id="nav">
+            <li><a href="/" target="_blank">Home</a></li>
+            <li><a href="/" target="_blank">All posts</a></li>
+        </ul>
+        <div class="button-container">
+            <button id="plus-btn"
+                v-on:click="clicker"
+                >Add new post</button>
+            <button
+                id="user-btn"
+                v-on:click="clicker"
+                >Log in/Register</button>
+        </div>
+        <Dropdown :options="options" v-if="dropdownState"/>
+    </header>
+</template>
+
+//script
+<script>
+    import Dropdown from './Dropdown.vue'
+
+    export default {
+        name: 'Header',
+        props: {},
+        data () {
+            return {
+                options: [
+                    {
+                        title: "Add new post",
+                        url: "my-posts"
+                    },
+                    {
+                        title: "Log in",
+                        url: "login"
+                    }
+                ],
+                dropdownState: false
+            }
+        },
+        components: {
+            Dropdown
+        },
+        methods: {
+         clicker () {
+            return this.dropdownState = !this.dropdownState
+        }
+        }     
+    }
+</script>
+
+//style
+<style>
+
+@import url('https://fonts.googleapis.com/css2?family=Comfortaa:wght@400;700&display=swap');
+    header {
+        display: flex;
+        background-color: #bb8760;
+        width: 100%;
+        justify-content: space-between;
+    }
+
+    #nav {
+        display: flex;
+        flex: 1;
+        justify-content: start;
+        list-style: none;
+        
+    }
+
+    #nav > li {
+        margin: 1rem 0rem;
+    }
+
+    #logo {
+       margin: 10px 30px;
+        width: 3.5rem;
+        height: 4.5rem;
+    }
+
+    .button-container {
+        display: flex;
+    }
+
+    #plus-btn, #user-btn, a {
+        background-color: #bb8760;
+        font-family: 'Comfortaa', cursive;
+        color:#4f0e0e;
+        position: relative;
+        border: none;
+        margin: 10px 10px; 
+        text-decoration: none;
+        font-size: 1rem;
+    }
+
+
+</style>
+
